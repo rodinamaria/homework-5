9ed4e316484ec43ba4d63a3c90ca0d564233f67f
mariarodina <q6m4?Qskk#>
2022-03-27 23:15:10 +0300

    дз 5.1. массивы 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write   int value = 10;
-        //        int [] values = {1, 5, 6, 8, 9};
-        //        System.out.println(values[2]);
-        //        values[3] = 18;
-        //        System.out.println(values[3]);your code here
 
-        // public static void main(String[] args) {
-        //        int[] values = {1, 5, 7, 9, 11};
-        //        printArray(values);
-        //        printArrayReverse(values);
-        //    }
-        //
-        //    public static void printArrayReverse(int[] values){
-        //        for (int i = values.length - 1; i >= 0 ; i--) {
-        //            System.out.print(values[i] + " ");
-        //        }
-        //        System.out.println();
-        //    }
-        //    public static void printArray(int[] values) {
-        //        for (int i = 0; i < values.length; i++) {
-        //            System.out.print(values[i] + " ");
-        //        }
-        //        System.out.println();
-        //    }
-
+        // Задачи 1-2-3
 //
         int[] numbers = new int[]{1, 2, 3};
         for (int i = 0; i < numbers.length; i++) {
@@ -48,6 +25,14 @@ public class Main {
             }
         }
         System.out.println();
+
+        // цикл для задачи 4
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0)
+            { System.out.print(numbers [i] + 1 + " ");
+            }
+        }
+        System.out.println();
 //
         double[] doubles = new double[]{1.57, 7.654, 9.986};
         for (int i = 0; i < doubles.length; i++) {
@@ -56,34 +41,43 @@ public class Main {
             } else {
                 System.out.print(doubles[i]);
             }
-            System.out.println();
         }
+        System.out.println();
 
-            double d = doubles[doubles.length - 1];
-            for (; d > 0; d--) {
-                if (doubles.length - 1 > d) {
-                    System.out.print(d + " ");
-                } else {
-                    System.out.print(d + ", ");
-                }
-                System.out.println();
+        for (int i = doubles.length - 1; i >= 0; i--) {
+            if (i >= 1) {
+                System.out.print(doubles[i] + ", ");
+            } else {
+                System.out.print(doubles[i]);
             }
-
+        }
+        System.out.println();
 //
-            String[] seasons = new String[]{"winter", "spring", "summer", "autumn"};
-           for (int i = 0; i < seasons.length; i++) {
-                System.out.print(seasons[i] + ", ");
-            }
-            System.out.println();
-
-           for (int i = seasons.length - 1; i >= 0; i--) {
-                System.out.print(seasons[i] + ", ");
-            }
-            System.out.println();
+        String[] seasons = new String[]{"winter", "spring", "summer", "autumn"};
+        for (int i = 0; i < seasons.length; i++) {
+            System.out.print(seasons[i] + ", ");
         }
+        System.out.println();
 
-
+        for (int i = seasons.length - 1; i >= 0; i--) {
+            if (i >= 1) {
+                System.out.print(seasons[i] + ", ");
+            } else {
+                System.out.print(seasons[i]);
+            }
+        }
+        System.out.println();
     }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
